81f4c7859c6d76f11c1c4241e03e1048d763ae48
dnkelly97 <dnkelly97@gmail.com>
2021-02-18 15:51:29 -0600

    Created settings folder, moved settings.py into it and renamed settings.py to common.py. Created development.py and production.py, which import settings from common.py and store the settings that are different between development and production environments respectively.

--- PGSCM/PGSCM/settings/common.py
+++ PGSCM/PGSCM/settings/common.py
@@ -19,14 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j04#7k2y*#1y%)54it3h1ve^2#iafyq-%^d$6sw1ceg58j-m!h'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
---
+++ PGSCM/PGSCM/settings/development.py
@@ -0,0 +1,9 @@
+from PGSCM.settings.common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'j04#7k2y*#1y%)54it3h1ve^2#iafyq-%^d$6sw1ceg58j-m!h'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
