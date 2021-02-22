8e1c4dfb11403eadbe5b9c52ed651365126bd56f
Gabriel Douglass <gabrieljdouglass@gmail.com>
2021-02-22 12:41:24 -0600

    I moved the DATABASES entry that had been in the settings.common file to the settings.development file

--- PGSCM/PGSCM/settings/development.py
+++ PGSCM/PGSCM/settings/development.py
@@ -6,4 +6,15 @@ SECRET_KEY = 'j04#7k2y*#1y%)54it3h1ve^2#iafyq-%^d$6sw1ceg58j-m!h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'temppassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
 ALLOWED_HOSTS = []
