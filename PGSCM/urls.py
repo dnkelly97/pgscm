104f3502332d2b2ed32ddecf5958451b84f3dbda
dnkelly97 <dnkelly97@gmail.com>
2021-03-05 13:35:13 -0600

    import statement for pipeline views in PGSCM/urls.py

--- PGSCM/urls.py
+++ PGSCM/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from PGSCM.pipeline import views as pipeline_views
+from pipeline import views as pipeline_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
