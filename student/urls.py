5052593c297e323bfba3ac4f600f06019b524c08
Mathew Seng <mathewseng@gmail.com>
2021-03-04 22:19:50 -0600

    Redirect to create-student.html to create a new student

--- student/urls.py
+++ student/urls.py
@@ -1,6 +1,7 @@
-from django.urls import include, path
+from django.urls import path
 from student.views import createPage, homePage
 
 urlpatterns = [
-    path('', homePage, name='home')
+    path('', homePage, name='home'),
+    path('create/', createPage, name='create')
 ]
