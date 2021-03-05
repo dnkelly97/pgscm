a05385f634a954d3f068085b47b8e0ebd720371c
dnkelly97 <dnkelly97@gmail.com>
2021-03-05 17:09:01 -0600

    Required names to be unique for Pipeline and SavedQuery. Migrated change. Minor change to template.

--- pipeline/models.py
+++ pipeline/models.py
@@ -4,9 +4,9 @@ from django.db import models
 # Create your models here.
 
 class Pipeline(models.Model):
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=60, unique=True)
 
 
 class SavedQuery(models.Model):
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=60, unique=True)
 
