664d42a99cf8c1d9cc024f3401a6391a10ee56ca
dnkelly97 <dnkelly97@gmail.com>
2021-03-05 12:57:47 -0600

    Added a dashboard view and models for Pipeline and SavedQuery. These models will need to be filled out in the future, right now they are basically just placeholders.

--- PGSCM/pipeline/models.py
+++ PGSCM/pipeline/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Pipeline(models.Model):
+    name = models.CharField(max_length=60)
+
+
+class SavedQuery(models.Model):
+    name = models.CharField(max_length=60)
+
--- PGSCM/pipeline/views.py
+++ PGSCM/pipeline/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Pipeline
+from .models import SavedQuery
+
 
 # Create your views here.
+
+def dashboard(request):
+    pipelines = Pipeline.objects.all()
+    saved_queries = SavedQuery.objects.all()
+    return render(request, 'dashboard.html', {'pipelines': pipelines, 'saved_queries': saved_queries})
