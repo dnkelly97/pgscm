1da44f1960947d2083203580fb4fb52aeff21bc6
Gabriel Douglass <gabrieljdouglass@gmail.com>
2021-03-06 10:35:35 -0600

    Importing things to make our login view nice and functional

--- login/views.py
+++ login/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
 # Create your views here.
+from .models import *
+from .forms import OrderForm
+from .filters import OrderFilter
