c6194c705bab8d062cd31ce604241e4700756742
dnkelly97 <dnkelly97@gmail.com>
2021-03-06 12:45:31 -0600

    Wrote bdd tests for checking that pipelines and saved queries in database are displayed on the dashboard

--- test/steps/test_dashboard_display_steps.py
+++ test/steps/test_dashboard_display_steps.py
@@ -1,5 +1,6 @@
 from pytest_bdd import scenario, given, when, then
 import pytest
+from pipeline.models import Pipeline, SavedQuery
 
 
 @scenario("../feature/dashboard_display.feature", "I am on the dashboard page")
@@ -15,13 +16,20 @@ def dashboard_response(live_server, driver):
 
 
 @then("I should see existent pipelines")
+@pytest.mark.django_db
 def assert_existent_pipelines_displayed(dashboard_response):
-    pass
+    pipelines = Pipeline.objects.all()
+    pipeline_names = [pipeline.name for pipeline in pipelines]
+    for name in pipeline_names:
+        assert dashboard_response.contains(name)
 
 
 @then("I should see existent saved queries")
 def assert_existent_saved_queries_displayed(dashboard_response):
-    pass
+    saved_queries = SavedQuery.objects.all()
+    saved_query_names = [query.name for query in saved_queries]
+    for name in saved_query_names:
+        assert dashboard_response.contains(name)
 
 
 @then("I should see buttons for creating, deleting, and editing pipelines")
