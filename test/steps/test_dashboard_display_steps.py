685d57248094a0af42a1b2b40580d102a9d68ab9
dnkelly97 <dnkelly97@gmail.com>
2021-03-08 13:07:17 -0600

    Merged main's testing fixes into this branch, so I deleted steps/conftest in favor of test/conftest, and added registering of factories to test/conftest. I also modified my BDD tests to use the browser fixture (new name) instead of the driver fixture (old name).

--- test/steps/test_dashboard_display_steps.py
+++ test/steps/test_dashboard_display_steps.py
@@ -11,47 +11,47 @@ def test_dashboard_display(live_server):
 
 
 @given("I am on the dashboard")
-def dashboard_setup(live_server, driver, pipeline_factory, saved_query_factory):
+def dashboard_setup(live_server, browser, pipeline_factory, saved_query_factory):
     for i in range(3):
         pipeline_factory()
         saved_query_factory()
     # todo once login is done: driver.post(liveserver + '/login/')
-    driver.get(live_server + reverse('dashboard'))
+    browser.get(live_server + reverse('dashboard'))
 
 
 @then("I should see existent pipelines")
-def assert_existent_pipelines_displayed(driver):
+def assert_existent_pipelines_displayed(browser):
     pipelines = Pipeline.objects.all()
     pipeline_names = [pipeline.name for pipeline in pipelines]
     for name in pipeline_names:
-        assert name in driver.page_source
+        assert name in browser.page_source
 
 
 @then("I should see existent saved queries")
-def assert_existent_saved_queries_displayed(driver):
+def assert_existent_saved_queries_displayed(browser):
     saved_queries = SavedQuery.objects.all()
     saved_query_names = [query.name for query in saved_queries]
     for name in saved_query_names:
-        assert name in driver.page_source
+        assert name in browser.page_source
 
 
 @then("I should see buttons for creating, deleting, and editing pipelines")
-def assert_pipeline_buttons(driver):
-    assert driver.find_element_by_id('create_pipeline_button')
-    assert driver.find_element_by_id('delete_pipeline_button')
-    assert driver.find_element_by_id('edit_pipeline_button')
+def assert_pipeline_buttons(browser):
+    assert browser.find_element_by_id('create_pipeline_button')
+    assert browser.find_element_by_id('delete_pipeline_button')
+    assert browser.find_element_by_id('edit_pipeline_button')
 
 
 @then("I should see buttons for creating, deleting, and editing saved queries")
-def assert_saved_query_buttons(driver):
-    assert driver.find_element_by_id('create_query_button')
-    assert driver.find_element_by_id('delete_query_button')
-    assert driver.find_element_by_id('edit_query_button')
+def assert_saved_query_buttons(browser):
+    assert browser.find_element_by_id('create_query_button')
+    assert browser.find_element_by_id('delete_query_button')
+    assert browser.find_element_by_id('edit_query_button')
 
 @then("each pipeline and query should have a checkbox to select it")
-def assert_queries_and_pipelines_have_checkboxes(driver):
+def assert_queries_and_pipelines_have_checkboxes(browser):
     for pipeline in Pipeline.objects.all():
-        assert driver.find_element_by_id(pipeline.name + ' radio button')
+        assert browser.find_element_by_id(pipeline.name + ' radio button')
     for query in SavedQuery.objects.all():
-        assert driver.find_element_by_id(query.name + ' radio button')
+        assert browser.find_element_by_id(query.name + ' radio button')
 
