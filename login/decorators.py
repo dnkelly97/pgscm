ce28172143573423c8082880d9ef1ba51ac5d8a9
Amar Vukovic <ammaar_1999@hotmail.com>
2021-03-09 21:33:52 -0600

    Modifying decorators to be somewhat simplified at this stage in the project as there is not that much that needs to be done with regards to user roles.

--- login/decorators.py
+++ login/decorators.py
@@ -8,45 +8,42 @@ from django.shortcuts import redirect
 def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect('dashboard')
+            return redirect('home')
         else:
             return view_func(request, *args, **kwargs)
 
     return wrapper_func
 
 
-def allowed_users(allowed_roles=None):
-    if allowed_roles is None:
-        allowed_roles = []
+# With more user types we can end up using this, but it might not be very useful currently
 
-    def decorator(view_func):
-        def wrapper_func(request, *args, **kwargs):
-
-            group = None
-            if request.user.groups.exists():
-                group = request.user.groups.all()[0].name
-
-            if group in allowed_roles:
-                return view_func(request, *args, **kwargs)
-            else:
-                return HttpResponse('ERROR: You cannot access this page')
-
-        return wrapper_func
-
-    return decorator
+# def allowed_users(allowed_roles=None):
+#     if allowed_roles is None:
+#         allowed_roles = []
+#
+#     def decorator(view_func):
+#         def wrapper_func(request, *args, **kwargs):
+#
+#             group = None
+#             if request.user.groups.exists():
+#                 group = request.user.groups.all()[0].name
+#
+#             if group in allowed_roles:
+#                 return view_func(request, *args, **kwargs)
+#             else:
+#                 return HttpResponse('ERROR: You cannot access this page')
+#
+#         return wrapper_func
+#
+#     return decorator
 
 
 def admin_func(view_func):
     def wrapper_function(request, *args, **kwargs):
-        group = None
-        if request.user.groups.exists():
-            group = request.user.groups.all()[0].name
 
-        if group == 'administrator':
-            return redirect('dashboard')
-        elif group == 'admin' or request.user.is_superuser:
+        if request.user.is_superuser:
             return view_func(request, *args, **kwargs)
         else:
-            return redirect('login')
+            return redirect('home')
 
     return wrapper_function
