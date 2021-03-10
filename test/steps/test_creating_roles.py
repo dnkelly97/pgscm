19b82754e04d52d66cdf70161e04f51e22caf538
Amar Vukovic <ammaar_1999@hotmail.com>
2021-03-10 11:10:39 -0600

    Updating BDD step files to implement BDD testing.

--- test/steps/test_creating_roles.py
+++ test/steps/test_creating_roles.py
@@ -1,36 +1,48 @@
+from django.contrib.auth.models import User
 from pytest_bdd import scenario, given, when, then
+from selenium.webdriver.common.keys import Keys
 
 
 @scenario('../feature/creating_roles.feature', 'Access Create User Page as Admin')
-def test_user_roles_admin():
+def test_user_roles_admin(live_server):
     pass
 
 
 @given("I am an Admin")
-def admin_access(admin_user):
-    pass
+def admin_access(live_server, browser):
+    browser.get(live_server + '/')
+    user = User.objects.create_user('admin', 'admin@uiowa.edu', 'admin123456')
+    user.is_superuser = True
+    user.save()
+    browser.find_element_by_id('id_username').send_keys('admin')
+    browser.find_element_by_id('id_password').send_keys('admin123456', Keys.RETURN)
 
 
 @when("I want to access the 'register' page")
-def connect_register(admin_user):
-    pass
+def connect_register(live_server, browser):
+    browser.get(live_server + '/register')
 
 
 @then("I get redirected to 'register' page")
-def redirect_register():
-    pass
+def redirect_register(browser):
+    assert browser.find_element_by_id('id_register_form')
 
 
 @scenario('../feature/creating_roles.feature', 'Access Create User Page as Administrator')
-def test_user_roles_administrator():
+def test_user_roles_administrator(live_server):
     pass
 
 
 @given("I am an Administrator")
-def administrator_access(admin_client):
-    pass
+def administrator_access(live_server, browser):
+    browser.get(live_server + '/')
+    user = User.objects.create_user('administrator', 'administrator@uiowa.edu', 'admin123456')
+    user.is_superuser = False
+    user.save()
+    browser.find_element_by_id('id_username').send_keys('administrator')
+    browser.find_element_by_id('id_password').send_keys('admin123456', Keys.RETURN)
 
 
 @then("I get redirected back to 'dashboard' page")
-def redirect_dashboard():
-    pass
+def redirect_dashboard(browser):
+    assert browser.find_element_by_id('id_dashboard')
