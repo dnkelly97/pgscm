e65329007a7f23a038ae89c335c94bebc2a01fa7
Amar Vukovic <ammaar_1999@hotmail.com>
2021-03-09 21:57:52 -0600

    Testing for creating roles updated to reflect as it is written currently.

--- test/unit/test_creating_roles_unit.py
+++ test/unit/test_creating_roles_unit.py
@@ -4,8 +4,8 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from django.test import TestCase, Client
 
-path('register/', views.registerUser, name="register"),
-path('', views.dashboard, name="dashboard")
+path('register/', views.registerPage, name="register"),
+path('', views.home, name="home")
 
 
 class AdministratorView(TestCase):
@@ -21,7 +21,7 @@ class AdministratorView(TestCase):
         self.user.is_staff = False
         self.user.save()
         self.client.login(username=self.username, password=self.password)
-        response = self.client.get(reverse('dashboard'))
+        response = self.client.get(reverse('home'))
         assert response.status_code == 200
 
     def test_administrator_register(self):
@@ -52,7 +52,7 @@ class AdminView(TestCase):
         self.user.is_superuser = True
         self.user.save()
         self.client.login(username=self.username, password=self.password)
-        response = self.client.get(reverse('dashboard'))
+        response = self.client.get(reverse('home'))
         assert response.status_code == 200
 
     def test_admin_register(self):
