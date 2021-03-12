f56db0ec395ea0b0f46b1b1d2b0d84067e8dc7e9
Amar Vukovic <ammaar_1999@hotmail.com>
2021-03-11 18:35:05 -0600

    Working on implementing and fixing up the testing with Mathew. Working using Code With Me.

---
+++ test/feature/student/student_querying.feature
@@ -0,0 +1,14 @@
+Feature: Student Querying Interface
+  As an administrator
+  So that I can find student matching certain criteria
+  I want to be able to search for students based on available criteria
+
+  Scenario: List students currently in the system
+    Given I know some users are already in the system
+    When I go to the 'dashboard'
+    Then I should see the students currently in the system
+
+  Scenario: Query students based on specific attributes
+    Given I have some attributes to sort students by
+    When I go to the 'dashboard'
+    Then I should be able to filter students based on those attributes
--- test/unit/student/test_student_views.py
+++ test/unit/student/test_student_views.py
@@ -1,57 +1,66 @@
 import pytest
 from student.models import Student
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
 from django.urls import reverse
 
 
-# @pytest.fixture
-# def student():
-#     data = {
-#         'email': user,
-#         'first_name': obj.name,
-#         'last_name': obj.price
-#     }
-#
-#     form = ProductForm(data=data)
-#     yield form
+class StudentView(TestCase):
+    def setUp(self):
+        self.username = 'bob'
+        self.password = 'bobpass123'
+        self.email = 'bob@uiowa.edu'
+        self.client = Client()
+        self.user = User.objects.create_user(self.username, self.email, self.password)
+        self.user.save()
+        self.client.login(username=self.username, password=self.password)
 
-@pytest.mark.django_db
-def test_student_portal(client):
-    url = reverse('student')
-    response = client.get(url)
-    assert response.status_code == 200
+    @pytest.mark.django_db
+    def test_student_portal(self):
+        url = reverse('student')
+        response = self.client.get(url)
+        assert response.status_code == 200
 
-@pytest.mark.django_db
-def test_create_student(client):
-    url = reverse('create')
-    response = client.get(url)
-    assert response.status_code == 200
+    @pytest.mark.django_db
+    def test_create_student(self):
+        url = reverse('create')
+        response = self.client.get(url)
+        assert response.status_code == 200
 
+    @pytest.mark.django_db
+    def test_create_student_submit_valid(self):
+        values = {"email": "hello@gmail.com", "first_name": 'first', "last_name": 'last', "school_year": Student.YearInSchool.UNKNOWN,
+                  "ethnicity": Student.Ethnicity.UNKNOWN, "gender": Student.Gender.UNKNOWN}
+        self.student = Student.objects.create(email="hello1@gmail.com", first_name="first", last_name="second")
+        self.student.save()
+        url = reverse('create')
+        self.data = {
+            'email': values["email"],
+            'first_name': values["first_name"],
+            'last_name': values["last_name"],
+            'school_year': values["school_year"],
+            'ethnicity': values["ethnicity"],
+            'gender': values["gender"]
+        }
+        response = self.client.post(url, data=self.data)
+        assert response.status_code == 200
 
-@pytest.mark.django_db
-@pytest.mark.parametrize(
-   'email, first_name, last_name, school_year, ethnicity,gender, status_code', [
-       ("hello@gmail.com", 'first', 'last',Student.YearInSchool.UNKNOWN,
-        Student.Ethnicity.UNKNOWN,
-        Student.Gender.UNKNOWN, 200),
-       ("hello1@gmail.com", 'first',"second",Student.YearInSchool.UNKNOWN,Student.Ethnicity.UNKNOWN,Student.Gender.UNKNOWN, 302)
-   ]
-)
-
-@pytest.mark.django_db
-def test_create_student_submit(client,email,first_name,last_name,school_year,ethnicity,gender,status_code):
-    student = Student.objects.create(email="hello@gmail.com", first_name="first", last_name="second")
-    student.save()
-    url = reverse('create')
-    data={
-        'email': email,
-        'first_name': first_name,
-        'last_name': last_name,
-        'school_year': school_year,
-        'ethnicity': ethnicity,
-        'gender': gender
-    }
-    response = client.post(url,data=data)
-    assert response.status_code == status_code
-
-
+    @pytest.mark.django_db
+    def test_create_student_submit_invalid(self):
+        values = {"email": "hello1@gmail.com", "first_name": 'first', "last_name": 'second',
+                  "school_year": Student.YearInSchool.UNKNOWN,
+                  "ethnicity": Student.Ethnicity.UNKNOWN, "gender": Student.Gender.UNKNOWN}
+        self.student = Student.objects.create(email="hello1@gmail.com", first_name="first", last_name="second")
+        self.student.save()
+        url = reverse('create')
+        self.data = {
+            'email': values["email"],
+            'first_name': values["first_name"],
+            'last_name': values["last_name"],
+            'school_year': values["school_year"],
+            'ethnicity': values["ethnicity"],
+            'gender': values["gender"]
+        }
+        response = self.client.post(url, data=self.data)
+        assert response.status_code == 302
