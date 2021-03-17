33854dca853a42d12f1b588d051c928edaa6a9a1
Gabriel Douglass <gabrieljdouglass@gmail.com>
2021-03-17 17:49:07 -0500

    I added a Stage class to Pipeline models; Stage instance has attributes: pipeline (foreign key), name, stage_number, time_window, advancement_condition.
    Also, now when a pipeline is created, so are however many stage instances were specified in the num_stages field for the pipeline

--- pipeline/models.py
+++ pipeline/models.py
@@ -13,7 +13,31 @@ class Pipeline(models.Model):
         ]
     )
 
+    def save(self, *args, **kwargs):
+        created = not self.pk
+        super().save(*args, **kwargs)
+        if created:
+            for i in range(self.num_stages):
+                Stage.objects.create(name="Stage " + str(i + 1), stage_number=i, pipeline=self,
+                                     advancement_condition='none').save()
+
 
 class SavedQuery(models.Model):
     name = models.CharField(max_length=60, unique=True)
 
+
+class Stage(models.Model):
+    pipeline = models.ForeignKey(Pipeline, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    stage_number = models.IntegerField(
+        validators=[
+            MinValueValidator(1)
+        ]
+    )
+    time_window = models.IntegerField(
+        default=30,
+        validators=[
+            MinValueValidator(0)
+        ]
+    )
+    advancement_condition = models.CharField(max_length=100)
