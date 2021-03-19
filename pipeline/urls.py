a6e334430f4a9f25da9d60f374ff9d1a630cf48e
gabrieljdouglass <gabrieljosephdouglass@gmail.com>
2021-03-19 17:13:05 -0500

    url for the define_Stages page

--- pipeline/urls.py
+++ pipeline/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from . import views
-from pipeline.views import createPage
+from pipeline.views import createPage, defineStages
 
 
 urlpatterns = [
     path('create/', csrf_exempt(createPage), name='create_pipeline'),
+    path('define_stages/<str:pk>/', csrf_exempt(defineStages), name='define_stages'),
 ]
