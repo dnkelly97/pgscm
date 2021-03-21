e2c0fa670d9acb17cdfe03a78457126eb22b7511
Mathew Seng <mathewseng@gmail.com>
2021-03-21 16:02:38 -0500

    Make API key migrations for APIKey Model
    This makes the initial migration to the APIKey model.
    
    This inherits the AbstractAPIKey from the Django Rest Framework API. This adds the email variable to verify the api can be sent to a user when created (later in the project).

--- apis/models.py
+++ apis/models.py
@@ -1,4 +1,6 @@
 from django.db import models
-from rest_framework_api_key.models import APIKey
+from rest_framework_api_key.models import AbstractAPIKey
 
 # Create your models here.
+class APIKey(AbstractAPIKey):
+    email = models.EmailField(max_length=255, unique=True)
