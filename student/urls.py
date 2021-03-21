adda2534b50d73549792beb583c250e587a8a2a6
Mathew Seng <mathewseng@gmail.com>
2021-03-21 16:46:24 -0500

    Require csrf tokens for creating a student
    This was disabled because this was merged when the login functionality was not yet complete. To protect the student database it should be required.

--- student/urls.py
+++ student/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from student.views import createPage, student
 from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
     path('', views.student, name='student'),
-    path('create/', csrf_exempt(createPage), name='create'),
+    path('create/', views.createPage, name='create'),
     path('delete_student/<str:key>', views.deleteStudent, name='delete_student'),
     path('update_student/<str:key>', views.updateStudent, name='update_student')
 ]
