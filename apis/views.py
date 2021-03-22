5adbae158c6f698a31a48f1e3c1922d8c402a56e
Mathew Seng <mathewseng@gmail.com>
2021-03-21 23:42:49 -0500

    Create a decorators for validating admin
    This will verify the person trying to access the admin portal is indeed a admin user.
    
    In the future we can record these interactions as a security risk for the Admin to view.

--- apis/views.py
+++ apis/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import *
+from .decorators import admin_api_func
 
+@admin_api_func
 def apis(request):
     keys = APIKey.objects.order_by('-created')
     context = {'keys': keys}
