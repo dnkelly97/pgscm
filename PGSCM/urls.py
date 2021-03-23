be0e2af369bf1428a8b5a67412e8ddf7e1bb92fc
Amar Vukovic <ammaar_1999@hotmail.com>
2021-03-23 16:11:02 -0500

    Updating urls to enable different pictures and files to be saved within the program.

--- PGSCM/urls.py
+++ PGSCM/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from pipeline import views as pipeline_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,4 @@ urlpatterns = [
     path('', include('login.urls')),
     path('student/', include('student.urls')),
     path('pipeline/', include('pipeline.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
