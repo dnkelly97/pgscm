ff46f107560b2ce51ea8f7416e3042237452d7aa
dnkelly97 <dnkelly97@gmail.com>
2021-03-22 23:34:50 -0500

    Wrote javascript to handle clicking the delete saved query button and on the dashboard page and the delete ubtton on the confirmation popup. This javascript also handles changing the popup visuals depending on ajax call results and what query (and eventually pipeline) was selected.

--- static/js/pipeline/delete.js
+++ static/js/pipeline/delete.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
-        $("#delete_query_button").on('click', function(event) {
+        $("#delete_query_button").on('click', function(event){
+            updateConfirmDeleteMessage('query');
+        });
+});
+
+function updateConfirmDeleteMessage(type){
+    var selection = $('input[name=selected_'+type+']:checked', '#select_query_form').val();
+    if(selection){
+        $('#confirm_delete_message').html("<h6>Are you sure you want to delete " + type + " '" + selection + "'?</h6>");
+        document.getElementById('final_delete_button').style.display = 'block';
+        $('#escape_popup').html("Cancel");
+    }
+    else{
+        $('#confirm_delete_message').html("<h6>No "+ type +" selected.</h6>");
+        document.getElementById('final_delete_button').style.display = 'none';
+        $('#escape_popup').html("Close");
+    }
+    $('#delete_modal').modal();
+}
+
+$(document).ready(function() {
+        $("#final_delete_button").on('click', function(event) {
             console.log('here');
             console.log($('#select_query_form').serialize());
             $.ajax({ data: $('#select_query_form').serialize(),
@@ -7,8 +28,16 @@ $(document).ready(function() {
                     url: $(this).attr('action'),
                     success: function(response) {
                         console.log(response['html']);
+                        $('#escape_popup').html("Close");
                         if(response['success']){
                             $('#saved_query_menu').html(response['html']);
+                            document.getElementById('final_delete_button').style.display = 'none';
+                            $('#confirm_delete_message').html("<div class='alert alert-success'>Successfully deleted.</div>");
+                        }
+                        else{
+                            //this case should be impossible
+                            document.getElementById('final_delete_button').style.display = 'none';
+                            $('#confirm_delete_message').html("<div class='alert alert-danger'>There was an error - item not deleted.</div>");
                         }
                     },
                     error: function (request, status, error) {
