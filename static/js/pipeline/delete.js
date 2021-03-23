9f2b238b3fc3d1e4ba9b09844b06fdf85bd8c42c
dnkelly97 <dnkelly97@gmail.com>
2021-03-23 10:38:42 -0500

    Changed dashboard, pipeline_menu templates and delete.js to extend frontend delete functionality to pipelines.

--- static/js/pipeline/delete.js
+++ static/js/pipeline/delete.js
@@ -2,10 +2,13 @@ $(document).ready(function() {
         $("#delete_query_button").on('click', function(event){
             updateConfirmDeleteMessage('query');
         });
+        $("#delete_pipeline_button").on('click', function(event){
+            updateConfirmDeleteMessage('pipeline');
+        });
 });
 
 function updateConfirmDeleteMessage(type){
-    var selection = $('input[name=selected_'+type+']:checked', '#select_query_form').val();
+    var selection = $('input[name=selected_'+type+']:checked', '#select_'+type+'_form').val();
     if(selection){
         $('#confirm_delete_message').html("<h6>Are you sure you want to delete " + type + " '" + selection + "'?</h6>");
         document.getElementById('final_delete_button').style.display = 'block';
@@ -22,15 +25,25 @@ function updateConfirmDeleteMessage(type){
 $(document).ready(function() {
         $("#final_delete_button").on('click', function(event) {
             console.log('here');
-            console.log($('#select_query_form').serialize());
-            $.ajax({ data: $('#select_query_form').serialize(),
+            if($('#confirm_delete_message').html().includes('query')){
+                var form = '#select_query_form';
+                var url = '/pipeline/delete_query/';
+                var menu = '#saved_query_menu'
+            }
+            else if($('#confirm_delete_message').html().includes('pipeline')){
+                var form = '#select_pipeline_form';
+                var url = '/pipeline/delete_pipeline/'
+                var menu = '#pipeline_menu'
+            }
+
+            console.log($(form).serialize());
+            $.ajax({ data: $(form).serialize(),
                     type: 'post',
-                    url: $(this).attr('action'),
+                    url: url,
                     success: function(response) {
-                        console.log(response['html']);
                         $('#escape_popup').html("Close");
                         if(response['success']){
-                            $('#saved_query_menu').html(response['html']);
+                            $(menu).html(response['html']);
                             document.getElementById('final_delete_button').style.display = 'none';
                             $('#confirm_delete_message').html("<div class='alert alert-success'>Successfully deleted.</div>");
                         }
