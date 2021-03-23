dc0aa15f03facc9b790be1c514d5eb2456410b31
gabrieljdouglass <gabrieljosephdouglass@gmail.com>
2021-03-23 14:21:04 -0500

    added a url for the define_stages html; again, I am at an intermediate step and going forward this path might be removed in the hopes that there is no longer a page switch when defining stages after having created a pipeline

--- pipeline/urls.py
+++ pipeline/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from . import views
-from pipeline.views import createPage, defineStages
+from pipeline.views import createPage
 
 
 urlpatterns = [
     path('create/', csrf_exempt(createPage), name='create_pipeline'),
-    path('define_stages/<str:pk>/', csrf_exempt(defineStages), name='define_stages'),
+    path('define_stages/<str:pk>/', views.stagedefinition),
 ]
