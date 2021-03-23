851cf61347ae7c61d0107321bbc2507826f69521
dnkelly97 <dnkelly97@gmail.com>
2021-03-23 10:37:23 -0500

    Added a url for delete_pipeline view

--- pipeline/urls.py
+++ pipeline/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from . import views
-from pipeline.views import createPage, delete_query
+from pipeline.views import createPage, delete_query, delete_pipeline
 
 
 urlpatterns = [
     path('create/', csrf_exempt(createPage), name='create_pipeline'),
-    path('delete_query/', delete_query, name="delete_query")
+    path('delete_query/', delete_query, name="delete_query"),
+    path('delete_pipeline/', delete_pipeline, name="delete_pipeline"),
 ]
