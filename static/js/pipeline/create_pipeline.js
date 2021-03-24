f10f27a2ddbc838bc45688dcee3d77f5f3797aac
gabrieljdouglass <gabrieljosephdouglass@gmail.com>
2021-03-24 10:07:26 -0500

    added stuff to ajax call for create pipeline submit button to produce a modal upon button click

--- static/js/pipeline/create_pipeline.js
+++ static/js/pipeline/create_pipeline.js
@@ -6,7 +6,12 @@ $(document).ready(function() {
                     type: 'post',
                     url: '/pipeline/create/',
                     success: function(response) {
-                        console.log(response);
+                        console.log(response['success']);
+                        var action = document.getElementById('define_stages_form').action;
+                        action = action.replace('null', response['pipeline_id']);
+                        document.getElementById('define_stages_form').action = action;
+                        $('#define_stages_formholder').html(response['html']);
+                        $('#define_stages_modal').modal();
                     },
                     error: function (request, status, error) {
                          console.log(request.responseText);
