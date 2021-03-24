610d533246e6d3f673e06819590efdd02167d04c
gabrieljdouglass <gabrieljosephdouglass@gmail.com>
2021-03-24 10:05:46 -0500

    updated urls to include new view functions

--- pipeline/urls.py
+++ pipeline/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from . import views
-from pipeline.views import ajax_create_pipeline, build_pipeline_page
+from pipeline.views import ajax_create_pipeline, build_pipeline_page, define_stages
 
 
 urlpatterns = [
     path('build/', build_pipeline_page, name='build_pipeline'),
-    #path('define_stages/<str:pk>/', views.stagedefinition),
-    path('create/', ajax_create_pipeline, name='create_pipeline')
+    path('create/', ajax_create_pipeline, name='create_pipeline'),
+    path('update_stages/<str:pipeline_id>', define_stages, name='define_stages')
 ]
