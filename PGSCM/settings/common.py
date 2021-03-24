98b71cb8df38e9ee6844b6b9ae6f27c858f20aa2
Amar Vukovic <ammaar_1999@hotmail.com>
2021-03-24 10:14:33 -0500

    Moving media files to development.py from common.py as this approach is only for development and more needs to be added for this to work in production.

--- PGSCM/settings/common.py
+++ PGSCM/settings/common.py
@@ -121,5 +121,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/')
