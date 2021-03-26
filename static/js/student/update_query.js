ba65c29a114697006c6bd4702267737ad1076577
dnkelly97 <dnkelly97@gmail.com>
2021-03-25 22:36:47 -0500

    Wrote javascript and added import to dashboard page for submitting selected query to update query url when edit query button is clicked.

---
+++ static/js/student/update_query.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+        $("#edit_query_button").on('click', function(event){
+            event.preventDefault();
+            var selection = $('input[name=selected_query]:checked', '#select_query_form').val();
+            $('#select_query_form').attr('action', "/student/update_query/" + selection).submit();
+        });
+});
