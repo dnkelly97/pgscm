2b63c8d563d25ab4982621b6ca86733ad2cf5a90
Mathew Seng <mathewseng@gmail.com>
2021-03-26 13:35:10 -0500

    Create a test for a valid API that is passed through the user's credentials

---
+++ test/unit/apis/test_apis_post_views.py
@@ -0,0 +1,31 @@
+import pytest
+from apis.models import APIKey
+from django.urls import reverse
+from rest_framework.test import APIClient
+from student.models import Student
+
+@pytest.mark.django_db
+def test_student_portal_valid():
+    obj = APIKey(
+        name="tester",
+        email="tester@uiowa.edu",
+
+    )
+    key = APIKey.objects.assign_key(obj)
+    obj.save()
+
+    length = len(Student.objects.all())
+
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION='Api-Key ' + key)
+
+    data = {
+        'email': 'test@gmail.com',
+        'first_name': 'first',
+        'last_name': 'last'
+    }
+
+    response = client.post(reverse('create_student_json'),
+                           data, format='json')
+    assert response.status_code == 201
+    assert 1 == length+1
