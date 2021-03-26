60ccd08903754297913ba79a669b850c7798e2db
gabrieljdouglass <gabrieljosephdouglass@gmail.com>
2021-03-25 21:16:19 -0500

    test for defining stages

--- test/unit/pipeline/test_pipeline_views.py
+++ test/unit/pipeline/test_pipeline_views.py
@@ -1,5 +1,5 @@
 import pytest
-from pipeline.models import Pipeline
+from pipeline.models import Pipeline, Stage
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 
@@ -34,3 +34,23 @@ class PipelineView(TestCase):
         }
         response = self.client.post(url, data=self.data)
         assert response.status_code == 200
+
+    @pytest.mark.django_db
+    def test_define_stages_submit_valid(self):
+        self.pipeline = Pipeline.objects.create(name="gabrielspipeline", num_stages=1 )
+        self.pipeline.save()
+        values = {"name": "Stage 1", "stage_number": 1, "time_window": 30,
+                  "advancement_condition": Stage.ConditionsForAdvancement.NONE,
+                  "pipeline_id": self.pipeline.id}
+        self.stage = Stage.objects.create(name="Stage 1", stage_number=1, time_window=30, pipeline_id=self.pipeline.id)
+        self.stage.save()
+        url = reverse('define_stages', kwargs={'pipeline_id': 43})
+        self.data = {
+            'name': values["name"],
+            'stage_number': values["stage_number"],
+            'time_window': values["time_window"],
+            'advancement_condition': values["advancement_condition"],
+            'pipeline_id': values["pipeline_id"]
+        }
+        response = self.client.post(url, data=self.data)
+        assert response.status_code == 302
