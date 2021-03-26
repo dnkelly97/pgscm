1ff6f4afcac9ac6f92e20ae6e58b108228176421
dnkelly97 <dnkelly97@gmail.com>
2021-03-26 13:33:42 -0500

    Added js to update_query.js and run_saved_query.js to stop action if no query is selected.

--- static/js/student/run_saved_query.js
+++ static/js/student/run_saved_query.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
         $("#run_saved_query_button").on('click', function(event){
             event.preventDefault();
             var selection = $('input[name=selected_query]:checked', '#select_query_form').val();
-            $('#select_query_form').attr('action', "/student/run_query/" + selection).submit();
+            if(selection){
+                $('#select_query_form').attr('action', "/student/run_query/" + selection).submit();
+            }
         });
 });
