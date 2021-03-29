8dbb60fce243ad43a9aaa885701bd22d92f03607
dnkelly97 <dnkelly97@gmail.com>
2021-03-29 12:09:31 -0500

    Added a url for ajax request that will get stages dynamically for create pipeline form, based on the number of stages the user defines.

--- pipeline/urls.py
+++ pipeline/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from . import views
-from pipeline.views import ajax_create_pipeline, build_pipeline_page, define_stages, delete_query, delete_pipeline
+from pipeline.views import *
 
 
 urlpatterns = [
     path('build/', build_pipeline_page, name='build_pipeline'),
+    path('get_stages/', ajax_get_stages, name="get_stages"),
     path('create/', ajax_create_pipeline, name='create_pipeline'),
     path('update_stages/<str:pipeline_id>', define_stages, name='define_stages'),
     path('delete_query/', delete_query, name="delete_query"),
