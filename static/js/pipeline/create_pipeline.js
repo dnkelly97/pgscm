2b3557d4aec2e9f5033a292d97bae8ffa14c120c
dnkelly97 <dnkelly97@gmail.com>
2021-03-29 12:19:50 -0500

    Reverted the js event handler for create pipeline button click to its original response, and added a new event handler the triggers on a change of the num stages value that submits the num stages with an ajax query to the get_stages url

--- static/js/pipeline/create_pipeline.js
+++ static/js/pipeline/create_pipeline.js
@@ -2,16 +2,33 @@ $(document).ready(function() {
         $("#create_pipeline_submit_button").on('click', function(event) {
             event.preventDefault();
             console.log('hey! pipeline button clicked');
+            $.ajax({ data: $('#create_pipeline_form').serialize(),
+                    type: 'post',
+                    url: '/pipeline/create/',
+                    success: function(response) {
+                        console.log(response['success']);
+                        var action = document.getElementById('define_stages_form').action;
+                        action = action.replace('null', response['pipeline_id']);
+                        document.getElementById('define_stages_form').action = action;
+                        $('#define_stages_formholder').html(response['html']);
+                        $('#define_stages_modal').modal();
+                    },
+                    error: function (request, status, error) {
+                         console.log(request.responseText);
+                    }
+            });
+        });
+});
+
+$(document).ready(function() {
+        $("#id_num_stages").on('change', function(event) {
+            event.preventDefault();
+            console.log('hey! pipeline button clicked');
             $.ajax({ data: $('#id_num_stages').serialize(),
                     type: 'get',
                     url: '/pipeline/get_stages/',
                     success: function(response) {
-//                        console.log(response['success']);
-//                        var action = document.getElementById('define_stages_form').action;
-//                        action = action.replace('null', response['pipeline_id']);
-//                        document.getElementById('define_stages_form').action = action;
-//                        $('#define_stages_formholder').html(response['html']);
-//                        $('#define_stages_modal').modal();
+
                     },
                     error: function (request, status, error) {
                          console.log(request.responseText);
