9f34d5d35b8a99479e45f8eb0d43065b7a5e242e
dnkelly97 <dnkelly97@gmail.com>
2021-03-29 14:10:52 -0500

    Changed Stage model field num_stages to be postive integer (0 or greater) and added frontend validation that the number of stages is greater than zero.

--- static/js/pipeline/create_pipeline.js
+++ static/js/pipeline/create_pipeline.js
@@ -23,17 +23,27 @@ $(document).ready(function() {
 $(document).ready(function() {
         $("#id_num_stages").on('change', function(event) {
             event.preventDefault();
-            console.log('hey! pipeline button clicked');
-            $.ajax({ data: $('#id_num_stages').serialize(),
+            var num_stages = $('#id_num_stages');
+            if(num_stages.val() != '0'){
+                document.getElementById("message").style = "display: none;";
+                $.ajax({ data: num_stages.serialize(),
                     type: 'get',
                     url: '/pipeline/get_stages/',
                     success: function(response) {
-                        $("#define_stages").html(response['html'])
+                        $("#define_stages").html(response['html']);
+                        document.getElementById("define_stages").style = "display: block;";
                         document.getElementById("create_pipeline_submit_button").style = "display: block;";
                     },
                     error: function (request, status, error) {
                          console.log(request.responseText);
                     }
-            });
+                });
+            }
+            else {
+                $('#message').html("The number of stages must be greater than 0");
+                document.getElementById("message").style = "display: block;";
+                document.getElementById("create_pipeline_submit_button").style = "display: none;";
+                document.getElementById("define_stages").style = "display: none;";
+            }
         });
 });
