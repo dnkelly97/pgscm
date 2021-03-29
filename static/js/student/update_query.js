fef2206677e83704b209c0398600ed03cc389176
dnkelly97 <dnkelly97@gmail.com>
2021-03-29 10:58:28 -0500

    removed test print statements.

--- static/js/student/update_query.js
+++ static/js/student/update_query.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
         $("#edit_query_button").on('click', function(event){
-            console.log("edit query button clicked");
             event.preventDefault();
             var selection = $('input[name=selected_query]:checked', '#select_query_form').val();
             if(selection){
                 $('input[name="csrfmiddlewaretoken"]', '#select_query_form').remove();
-                console.log($('input[name="csrfmiddlewaretoken"]', '#select_query_form').val());
-                setTimeout(() => {  console.log("World!"); }, 2000);
-//                $('#select_query_form').attr('action', "/student/update_query/" + selection).submit();
+                $('#select_query_form').attr('action', "/student/update_query/" + selection).submit();
             }
         });
 });
