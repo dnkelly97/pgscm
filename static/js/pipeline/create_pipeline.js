60d5c03a858116c1105bc43d5a04bccdea187fda
dnkelly97 <dnkelly97@gmail.com>
2021-03-29 12:11:32 -0500

    Changed for create_pipeline button click to route to the new url for getting the form display for the number of stages the user defines. At this point the js is still activated by a button click of the create pipeline button, but that will change. I'm just using the existing click functionality to develop.

--- static/js/pipeline/create_pipeline.js
+++ static/js/pipeline/create_pipeline.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
         $("#create_pipeline_submit_button").on('click', function(event) {
             event.preventDefault();
             console.log('hey! pipeline button clicked');
-            $.ajax({ data: $('#create_pipeline_form').serialize(),
-                    type: 'post',
-                    url: '/pipeline/create/',
+            $.ajax({ data: $('#id_num_stages').serialize(),
+                    type: 'get',
+                    url: '/pipeline/get_stages/',
                     success: function(response) {
-                        console.log(response['success']);
-                        var action = document.getElementById('define_stages_form').action;
-                        action = action.replace('null', response['pipeline_id']);
-                        document.getElementById('define_stages_form').action = action;
-                        $('#define_stages_formholder').html(response['html']);
-                        $('#define_stages_modal').modal();
+//                        console.log(response['success']);
+//                        var action = document.getElementById('define_stages_form').action;
+//                        action = action.replace('null', response['pipeline_id']);
+//                        document.getElementById('define_stages_form').action = action;
+//                        $('#define_stages_formholder').html(response['html']);
+//                        $('#define_stages_modal').modal();
                     },
                     error: function (request, status, error) {
                          console.log(request.responseText);
