e8df50ae95801b2527b4f334c273b9c850932bbd
dnkelly97 <dnkelly97@gmail.com>
2021-03-29 10:57:54 -0500

    For the run_saved_query.js and update_query.js event listeners, which run when the 'Run Query' or 'Edit Query' buttons are clicked, I added a line that removes the csrfmiddlewaretoken from the DOM. The csrf token should not be included in forms when they are submitted with GET requests. However, I could not just remove the {% csrf_token %} tag from the template because the saved_query form is sometimes submitted with a POST request, which requires the csrf token. This will come up again with the edit pipeline user story.

--- static/js/student/update_query.js
+++ static/js/student/update_query.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
         $("#edit_query_button").on('click', function(event){
+            console.log("edit query button clicked");
             event.preventDefault();
             var selection = $('input[name=selected_query]:checked', '#select_query_form').val();
             if(selection){
-                $('#select_query_form').attr('action', "/student/update_query/" + selection).submit();
+                $('input[name="csrfmiddlewaretoken"]', '#select_query_form').remove();
+                console.log($('input[name="csrfmiddlewaretoken"]', '#select_query_form').val());
+                setTimeout(() => {  console.log("World!"); }, 2000);
+//                $('#select_query_form').attr('action', "/student/update_query/" + selection).submit();
             }
         });
 });
