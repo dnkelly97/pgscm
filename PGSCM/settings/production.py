bb71c30d967c04e9bbc4dddd471348cb8d2a70d5
Amar Vukovic <ammaar_1999@hotmail.com>
2021-03-31 12:07:53 -0500

    Updating production.py after experimentation within development environment. This will hopefully work now.

--- PGSCM/settings/production.py
+++ PGSCM/settings/production.py
@@ -31,10 +31,11 @@ DATABASES = {
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_S3_REGION_NAME = 'us-east-2'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'PGSCM.CustomStorage.CustomS3Boto3Storage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/')
 
