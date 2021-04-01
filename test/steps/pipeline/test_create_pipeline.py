a4573f51a01d20f729f31152c8f919c6075555e6
dnkelly97 <dnkelly97@gmail.com>
2021-03-31 21:39:06 -0500

    Modified existing acceptance test for successful creation of pipeline

--- test/steps/pipeline/test_create_pipeline.py
+++ test/steps/pipeline/test_create_pipeline.py
@@ -1,8 +1,9 @@
 from pytest_bdd import scenario, given, when, then
 import pytest
-from django.contrib.auth.models import User
-from selenium.webdriver.common.keys import Keys
 from django.urls import reverse
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.mark.django_db
@@ -14,7 +15,7 @@ def test_change_num_stages(logged_in_browser):
 @when("I change the number of stages")
 def change_num_stages(logged_in_browser):
     logged_in_browser.find_element_by_id("id_num_stages").send_keys('2')
-    logged_in_browser.find_element_by_id("id_num_stages").send_keys(Keys.RETURN)
+    logged_in_browser.find_element_by_id("id_name").click()  # click elsewhere on the page to produce change event on num_stages
 
 
 @then("I should see fields for creating a stage equal to the number of stages selected")
@@ -27,7 +28,8 @@ def assert_fields_for_correct_num_stages(logged_in_browser):
 @pytest.mark.parametrize(
     ['name', 'num_stages'],
     [
-        ('t', 1)
+        ('t', 1),
+        ('t2', 2)
     ]
 )
 @scenario("../../feature/pipeline/create_pipeline.feature",
@@ -47,15 +49,17 @@ def fill_out_name(browser, name):
 
 
 @when("I fill out number of stages: <num_stages>")
-def fill_out_name(browser, num_stages):
-    browser.find_element_by_id('id_num_stages').send_keys(num_stages)
+def fill_out_name(logged_in_browser, num_stages):
+    logged_in_browser.find_element_by_id('id_num_stages').send_keys(num_stages)
+    logged_in_browser.find_element_by_id("id_name").click()  # click elsewhere on the page to produce change event on num_stages
 
 
 @when("I click the create pipeline submit button")
-def create_pipeline_submit(browser):
-    browser.find_element_by_id('create_pipeline_submit_button').click()
+def create_pipeline_submit(logged_in_browser):
+    logged_in_browser.find_element_by_id('create_pipeline_submit_button').click()
 
 
 @then("I should be on the dashboard")
-def assert_create_student_submit(browser):
-    assert browser.find_element_by_id('create_pipeline_submit_button')
+def assert_on_dashboard(logged_in_browser):
+    WebDriverWait(logged_in_browser, 10).until(
+        EC.presence_of_element_located((By.ID, "create_query_button")))
