2cdf9d03e48587762e7dbbf4b36182f7657a01db
dnkelly97 <dnkelly97@gmail.com>
2021-03-31 21:02:13 -0500

    Removed obsolete create pipeline views and urls

--- pipeline/urls.py
+++ pipeline/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('build/', build_pipeline_page, name='build_pipeline'),
     path('get_stages/', ajax_get_stages, name="get_stages"),
     path('create/', create_pipeline, name='create_pipeline'),
-    path('update_stages/<str:pipeline_id>', define_stages, name='define_stages'),
     path('delete_query/', delete_query, name="delete_query"),
     path('delete_pipeline/', delete_pipeline, name="delete_pipeline"),
 ]
--- pipeline/views.py
+++ pipeline/views.py
@@ -61,41 +61,6 @@ def create_pipeline(request):
 
 
 @login_required(login_url='login')
-def ajax_create_pipeline(request):
-    # breakpoint()
-    form = CreatePipelineForm(request.POST)
-    pipeline = Pipeline.objects.all().order_by('id').last()
-    if form.is_valid():
-        success = True
-        pipeline = form.save()
-        stages = Stage.objects.filter(pipeline=pipeline.id)
-        stageforms = []
-        for i in range(len(stages)):
-            instance = stages.filter(stage_number=i).first()
-            stageforms.append(UpdateStageForm(instance=instance))
-        partial = render_to_string('define_stages.html', {'forms': stageforms, 'stages': stages})
-    else:
-        partial = None
-        success = False
-    return JsonResponse({'success': success, 'html': partial, 'pipeline_id': pipeline.id})
-
-
-@login_required(login_url='login')
-def define_stages(request, pipeline_id):
-    stages = Stage.objects.filter(pipeline=pipeline_id)
-    success = True
-    post = dict(request.POST)
-    for i in range(len(stages)):
-        fields = {'name': post['name'][i], 'stage_number': i+1, 'time_window': post['time_window'][i], 'advancement_condition': post['advancement_condition'][i], 'pipeline': pipeline_id}
-        form = UpdateStageForm(fields, instance=stages[i])
-        if form.is_valid():
-            stage = form.save()
-        else:
-            success = False
-    return redirect(reverse('dashboard'))
-
-
-@login_required(login_url='login')
 def delete_query(request):
     try:
         SavedQuery.objects.get(query_name=request.POST['selected_query']).delete()
