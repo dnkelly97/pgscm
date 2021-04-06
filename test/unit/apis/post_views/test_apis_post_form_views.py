1bbf98d1def995c3d2cac7b3b7fba44e891f4acf
dnkelly97 <dnkelly97@gmail.com>
2021-04-06 17:55:19 -0500

    Added test for form submission, same as json submission but with form as content type.

--- test/unit/apis/post_views/test_apis_post_form_views.py
+++ test/unit/apis/post_views/test_apis_post_form_views.py
@@ -2,7 +2,7 @@ import pytest
 from apis.models import APIKey
 from django.urls import reverse
 from student.models import Student
-from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIRequestFactory, APIClient
 from apis.post_views import form_view
 
 @pytest.mark.django_db
@@ -107,3 +107,41 @@ def test_student_portal_invalid(email,first_name,last_name,code):
     response = form_view(request)
 
     assert response.status_code == code
+
+
+@pytest.mark.django_db
+def test_api_add_student_with_some_extended_fields():
+    obj = APIKey(
+        name="tester",
+        email="tester@uiowa.edu",
+    )
+    key = APIKey.objects.assign_key(obj)
+    obj.save()
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION='Api-Key ' + key)
+    data = {
+        'email': 'yes@gmail.com',
+        'first_name': 'boz',
+        'last_name': 'scaggs',
+        'school_year': 'SR',
+        'research_interests': ['AI', 'Medical Imaging', 'Art of Dance'],
+        'gpa': 4.1,
+        'military': True
+    }
+    response = client.post(reverse('create_student_form'), data)
+    assert response.status_code == 201
+    new_student = Student.objects.get(first_name='boz')
+    assert new_student.first_name == "boz"
+    assert new_student.last_name == "scaggs"
+    assert new_student.email == "yes@gmail.com"
+    assert new_student.school_year == "SR"
+    assert new_student.research_interests == ['AI', 'Medical Imaging', 'Art of Dance']
+    assert new_student.degree == ""
+    assert new_student.gpa == 4.1
+    assert new_student.military
+    assert new_student.gender == 'U'
+    assert new_student.ethnicity == 'U'
+    assert new_student.country == ""
+    assert not new_student.first_generation
+    assert new_student.university == ""
+    assert not new_student.us_citizenship
