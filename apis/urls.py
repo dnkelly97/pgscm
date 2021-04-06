ce10e64e9f28135d1f3bba8974c65502fdc06edf
Amar Vukovic <ammaar_1999@hotmail.com>
2021-04-05 21:50:09 -0500

    Updating urls.py in order to address changes in the way pages are routed.

--- apis/urls.py
+++ apis/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.apis, name='api'),
     path('create/', views.createAPI, name='create_api'),
     path('api_profile/<str:key>', views.apiProfile, name='api_profile'),
-    path('create_json/', post_views.json_view, name='create_student_json')
+    path('create_json/', post_views.json_view, name='create_student_json'),
+    path('create_form/', post_views.form_view, name='create_student_form')
 ]
