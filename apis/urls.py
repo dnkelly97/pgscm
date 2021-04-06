5a7b5fd42176f8e2404ad038edc3ac676af58e4a
Mathew Seng <mathewseng@gmail.com>
2021-04-06 11:49:56 -0500

    Create api delete url

--- apis/urls.py
+++ apis/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('create/', views.apis, name='create_api'),
     path('api_profile/<str:key>', views.apiProfile, name='api_profile'),
     path('regenerate_api/', views.ajax_api_regenerate, name='regenerate_api'),
+    path('delete_api/', views.ajax_api_delete, name='delete_api'),
     path('update_api/<str:key>', views.apiUpdate, name='update_api'),
     path('create_json/', post_views.json_view, name='create_student_json'),
     path('create_form/', post_views.form_view, name='create_student_form')
