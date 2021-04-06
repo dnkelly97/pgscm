0e8f1ce57d99c6f7d7eb4380750e8d1b89e3d9f3
Mathew Seng <mathewseng@gmail.com>
2021-04-06 09:50:21 -0500

    New view for generating new key

--- apis/views.py
+++ apis/views.py
@@ -41,6 +41,21 @@ def apiProfile(request, key):
     return render(request, 'api_profile.html', context)
 
 @admin_api_func
+def ajax_api_regenerate(request):
+    try:
+        key = APIKey.objects.get(prefix=request.POST['prefix'])
+        new_key = key
+        key.delete()
+        APIKey.objects.assign_key(new_key)
+        new_key.save()
+        partial = render_to_string('api_profile.html', context, request)
+        success = True
+    except APIKey.DoesNotExist:
+        partial = None
+        success = False
+    return JsonResponse({'success': success, 'html': partial})
+
+@admin_api_func
 def apiUpdate(request, key):
     api_keys = APIKey.objects.order_by('-created')
     api_key = APIKey.objects.get(prefix=key)
