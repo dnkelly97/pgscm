031ed83663528ffecdca6079dc36e87acdfec09d
Amar Vukovic <ammaar_1999@hotmail.com>
2021-04-05 23:02:11 -0500

    Updating views.py following removal of create page.

--- apis/views.py
+++ apis/views.py
@@ -4,6 +4,7 @@ from .models import *
 from .decorators import admin_api_func
 from .forms import CreateForm
 
+
 @admin_api_func
 def apis(request):
     keys = APIKey.objects.order_by('-created')
@@ -30,23 +31,6 @@ def apis(request):
     context = {'keys': keys, 'form': form}
     return render(request, 'api_home.html', context)
 
-@admin_api_func
-def createAPI(response):
-    if response.method == 'POST':
-        form = CreateForm(response.POST)
-
-        if form.is_valid():
-            obj = form.save(commit=False)
-            APIKey.objects.assign_key(obj)
-            obj.save()
-            response = redirect('api')
-            return response
-        context = {'form': form}
-        return render(response, 'create_api.html', context)
-
-    form = CreateForm
-    context = {'form': form}
-    return render(response, 'create_api.html', context)
 
 @admin_api_func
 def apiProfile(request, key):
