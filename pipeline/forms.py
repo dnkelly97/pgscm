524b79e9453b60a627b562d30addd45ee05e055b
gabrieljdouglass <gabrieljosephdouglass@gmail.com>
2021-04-06 14:20:42 -0500

    to make the widget a sequence of check boxes I had to define __init__ in the create pipeline form. This is still an intermediate of the final product as there is still some tweaking to do

--- pipeline/forms.py
+++ pipeline/forms.py
@@ -1,6 +1,6 @@
-from django.forms import ModelForm
-from pipeline.models import Pipeline, Stage
-from django.forms import ModelForm, Textarea
+from django.forms import forms
+from pipeline.models import Pipeline, Stage, SavedQuery
+from django.forms import ModelForm, Textarea, widgets
 
 
 class CreatePipelineForm(ModelForm):
@@ -11,6 +11,12 @@ class CreatePipelineForm(ModelForm):
         widgets = {
             'description': Textarea(attrs={'rows': 3, 'cols': 40})
         }
+
+    def __init__(self, *args, **kwargs):
+        super(CreatePipelineForm, self).__init__(*args, **kwargs)
+        self.fields["source"].widget = widgets.CheckboxSelectMultiple()
+        self.fields["source"].help_text = ""
+        self.fields["source"].queryset = SavedQuery.objects.all()
 
 
 class UpdateStageForm(ModelForm):
