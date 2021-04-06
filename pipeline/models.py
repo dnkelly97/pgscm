1d22605fae88f82ae0f4f8473e9d5e3f294f851d
gabrieljdouglass <gabrieljosephdouglass@gmail.com>
2021-04-06 14:21:23 -0500

    made it so that the source attribute of the pipeline is a manytomany field instead of a foreignkey field

--- pipeline/models.py
+++ pipeline/models.py
@@ -16,7 +16,7 @@ class SavedQuery(models.Model):
 
 
 class Pipeline(models.Model):
-    source = models.ForeignKey(SavedQuery, on_delete=models.CASCADE, null=True, blank=True)
+    source = models.ManyToManyField(SavedQuery, blank=True)
     name = models.CharField(max_length=60, unique=True)
     description = models.TextField(blank=True, null=True)
     num_stages = models.PositiveIntegerField(
