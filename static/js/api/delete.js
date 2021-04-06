ae45527f09611094ac40f86689766e0569c20142
Mathew Seng <mathewseng@gmail.com>
2021-04-06 11:48:58 -0500

    Create delete ajax and redirect to api hope page on success

---
+++ static/js/api/delete.js
@@ -0,0 +1,41 @@
+function deleteAPIKey(name){
+    $('#confirm_delete_message').html("<h6>Are you sure you want to delete: " + name + "?</h6>");
+    document.getElementById('final_delete_button').style.display = 'block';
+    $('#escape_popup').html("Cancel");
+    $('#delete_modal').modal();
+}
+
+$(document).ready(function() {
+     $("#final_delete_button").on('click', function(event) {
+            console.log('here');
+            console.log('prefix');
+
+            var url = '/apis/delete_api/';
+            var form = '#delete_api_form';
+
+            console.log($(form).serialize());
+
+            $.ajax({
+                    data: $(form).serialize(),
+                    type: 'post',
+                    url: url,
+                    success: function(response) {
+                        $('#escape_popup').html("Close");
+                        if(response['success']){
+                            //$(menu).html(response['html']);
+                            document.getElementById('final_regenerate_button').style.display = 'none';
+                            $('#confirm_delete_message').html("<div class='alert alert-success'>Successfully regenerated.</div>");
+                            window.location.href = response['url']
+                        }
+                        else{
+                            //this case should be impossible unless caching is involved
+                            document.getElementById('final_delete_button').style.display = 'none';
+                            $('#confirm_delete_message').html("<div class='alert alert-danger'>There was an error - item not regenerated.</div>");
+                        }
+                    },
+                    error: function (request, status, error) {
+                         console.log(request.responseText);
+                    }
+           });
+    });
+});
