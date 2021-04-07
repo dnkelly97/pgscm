a4777b1e75cf776ae9cdd34597ad372a913603d3
gabrieljdouglass <gabrieljosephdouglass@gmail.com>
2021-04-07 17:06:00 -0500

    this was just a name change of source to sources to reflect that there can be multiple savedquerys associated with each pipeline

--- pipeline/models.py
+++ pipeline/models.py
@@ -16,7 +16,7 @@ class SavedQuery(models.Model):
 
 
 class Pipeline(models.Model):
-    source = models.ManyToManyField(SavedQuery, blank=True)
+    sources = models.ManyToManyField(SavedQuery, blank=True)
     name = models.CharField(max_length=60, unique=True)
     description = models.TextField(blank=True, null=True)
     num_stages = models.PositiveIntegerField(
