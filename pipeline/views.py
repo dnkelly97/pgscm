691b95fdea56f6a502d6e17847af27e445949f98
gabrieljdouglass <gabrieljosephdouglass@gmail.com>
2021-04-07 17:07:49 -0500

    when I switched from ForeignKey to ManyToMany the form was not submitting correctly for the longest time, and I realized it was because I was accessing source like post['sources'][0] when I needed to access it like this 'sources': post['sources']

--- pipeline/views.py
+++ pipeline/views.py
@@ -38,7 +38,7 @@ def ajax_get_stages(request):
 @login_required(login_url='login')
 def create_pipeline(request):
     post = dict(request.POST)
-    pipeline_info = {'name': post['name'].pop(0), 'description': post['description'][0], 'source': post['source'][0], 'num_stages': post['num_stages'][0]}
+    pipeline_info = {'name': post['name'].pop(0), 'description': post['description'][0], 'sources': post['sources'], 'num_stages': post['num_stages'][0]}
     pipeline_form = CreatePipelineForm(pipeline_info)
     if pipeline_form.is_valid():
         pipeline = pipeline_form.save()
