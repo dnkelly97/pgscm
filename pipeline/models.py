f5f9cbfd846e2afb03b207bae8e260aa0bf0ac24
gabrieljdouglass <gabrieljosephdouglass@gmail.com>
2021-04-07 20:13:19 -0500

    Sources can be null

--- pipeline/models.py
+++ pipeline/models.py
@@ -16,7 +16,7 @@ class SavedQuery(models.Model):
 
 
 class Pipeline(models.Model):
-    sources = models.ManyToManyField(SavedQuery, blank=True)
+    sources = models.ManyToManyField(SavedQuery, blank=True, null=True)
     name = models.CharField(max_length=60, unique=True)
     description = models.TextField(blank=True, null=True)
     num_stages = models.PositiveIntegerField(
