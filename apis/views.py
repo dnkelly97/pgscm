805bc58cd4006de4736ccc8e80e19950a32645f9
Mathew Seng <mathewseng@gmail.com>
2021-04-15 14:04:58 -0500

    Call emailer for regeneration, new key creation, and updating

--- apis/views.py
+++ apis/views.py
@@ -5,9 +5,7 @@ from .decorators import admin_api_func
 from .forms import CreateForm
 from django.http import JsonResponse
 from django.urls import reverse
-from django.core.mail import send_mail,BadHeaderError
-from smtplib import SMTPException
-from django.conf import settings
+import sys
 
 
 @admin_api_func
@@ -21,22 +19,10 @@ def apis(request):
             obj = form.save(commit=False)
             key = APIKey.objects.assign_key(obj)
 
-            try:
-                send_mail(subject="App Permissions",
-                          message="Your API key is: " + key +"\n\n"+
-                                  "This will only be sent to you once, so please store in a safe place." +
-                                  "If your API key expires or you lose it, please contact the Admin immediately.",
-                          from_email=settings.EMAIL_HOST_USER,
-                          recipient_list=[obj.email],
-                          fail_silently=False)
+            object = obj.save()
 
-            except BadHeaderError:  # If mail's Subject is not properly formatted.
-                print('Invalid header found.')
-            except SMTPException as e:  # It will catch other errors related to SMTP.
-                print('There was an error sending an email.' + str(e))
-            except:  # It will catch All other possible errors.
-                print("Mail Sending Failed!")
-            obj.save()
+            APIKey.send_email(object, key, obj.email)
+
             response = redirect('api')
             messages.success(request, 'Creation successful...')
             return response
@@ -66,8 +52,9 @@ def ajax_api_regenerate(request):
         key = APIKey.objects.get(prefix=request.POST['prefix'])
         new_key = key
         key.delete()
-        APIKey.objects.assign_key(new_key)
-        new_key.save()
+        new_client_key = APIKey.objects.assign_key(new_key)
+        object = new_key.save()
+        APIKey.send_email(object, new_client_key, new_key.email)
         success = True
         url = '/apis/api_profile/'+new_key.prefix
     except APIKey.DoesNotExist:
@@ -79,7 +66,6 @@ def ajax_api_regenerate(request):
 def ajax_api_delete(request):
     try:
         key = APIKey.objects.get(prefix=request.POST['prefix'])
-        name = key.name
         key.delete()
         success = True
         url = '/apis/'
@@ -100,8 +86,9 @@ def apiUpdate(request, key):
         if(obj.email != email):
             new_obj = obj
             obj.delete()
-            APIKey.objects.assign_key(new_obj)
-            new_obj.save()
+            key = APIKey.objects.assign_key(new_obj)
+            object = new_obj.save()
+            APIKey.send_email(object, key, obj.email)
             return redirect(reverse('api_profile', kwargs={'key':new_obj.prefix}))
 
         return redirect(reverse('api_profile', kwargs={'key':obj.prefix}))
