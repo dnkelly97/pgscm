d03c59dab4e05cf70356b89f6975683f05a4addb
gabrieljdouglass <gabrieljosephdouglass@gmail.com>
2021-04-16 13:50:40 -0500

    these are global rate limiters, and we want to control the scope of what we are rate limiting. In the case that these are only working on the API app's view, then I might return to this implementation. For now, I am removing it and looking for a more class scoped rate limiter, which I saw in the documentation right under the global rate limiters

--- PGSCM/settings/common.py
+++ PGSCM/settings/common.py
@@ -52,15 +52,7 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         "apis.permissions.HasAPIKey",
-    ],
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle'
-    ],
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/day',
-        'user': '1000/day'
-    }
+    ]
 
 }
 
