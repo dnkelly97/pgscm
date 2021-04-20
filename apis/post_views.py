d732a53a9ee26a3a9ff6b0556aa2a90f238de833
gabrieljdouglass <gabrieljosephdouglass@gmail.com>
2021-04-20 17:41:04 -0500

    These lines of code make it so that the functions associated with creating a student via json post request and form post request are rate limited. The limit is set to django's default rate limit for anonymous users.

--- apis/post_views.py
+++ apis/post_views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from rest_framework.parsers import JSONParser, FormParser, MultiPartParser
 from apis.permissions import HasAPIKey
-from rest_framework.decorators import api_view
-from rest_framework.decorators import parser_classes,permission_classes
+from rest_framework.decorators import api_view, action
+from rest_framework.decorators import parser_classes, permission_classes
 from .models import *
 from django.http.response import JsonResponse
 from .serializers import StudentSerializer
 from rest_framework import status
+from rest_framework.throttling import AnonRateThrottle
+
 
 @api_view(['POST'])
 @parser_classes([JSONParser])
 @permission_classes([HasAPIKey])
+@action(detail=True, methods=["post"], throttle_classes=[AnonRateThrottle])
 def json_view(request, format=None):
     key = request.META["HTTP_AUTHORIZATION"].split()[1]
     api_key = APIKey.objects.get_from_key(key)
@@ -23,9 +26,11 @@ def json_view(request, format=None):
     else:
         return JsonResponse("No API In Database", safe=False, status=status.HTTP_400_BAD_REQUEST)
 
+
 @api_view(['POST'])
-@parser_classes([FormParser,MultiPartParser])
+@parser_classes([FormParser, MultiPartParser])
 @permission_classes([HasAPIKey])
+@action(detail=True, methods=["post"], throttle_classes=[AnonRateThrottle])
 def form_view(request, format=None):
     key = request.META["HTTP_AUTHORIZATION"].split()[1]
     api_key = APIKey.objects.get_from_key(key)
