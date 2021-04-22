1168f004d4059a10c89a40117bc51ef70665836a
Mathew Seng <mathewseng@gmail.com>
2021-04-22 16:42:53 -0500

    Implement authorization headers for pipeline requests unit tests

--- test/unit/pipeline/test_dispatch_requests.py
+++ test/unit/pipeline/test_dispatch_requests.py
@@ -5,23 +5,26 @@ from pipeline.management.dispatch.dispatch_requests import *
 def test_dispatch_url():
     assert DISPATCH_URL == "http://127.0.0.1:8001"
 
-def test_json_request(httpserver):
-    httpserver.expect_request("/templates/").respond_with_json(["http://127.0.0.1:8001/templates/1"])
+def test_dispatch_auth():
+    assert DISPATCH_AUTH != "https://dispatchlite.azurewebsites.net/"
+
+def test_json_request(httpserver,authorization_header):
+    httpserver.expect_request("/templates/", headers=authorization_header).respond_with_json(["http://127.0.0.1:8001/templates/1"])
     assert get_templates() == ["http://127.0.0.1:8001/templates/1"]
 
-def test_get_template(httpserver):
-    httpserver.expect_request("/templates/1").respond_with_json({'content': 'yourmom'})
+def test_get_template(httpserver,authorization_header):
+    httpserver.expect_request("/templates/1", headers=authorization_header).respond_with_json({'content': 'yourmom'})
     assert get_template(httpserver.url_for("/templates/1")) == {'content': 'yourmom'}
 
-def test_get_all_template_data(httpserver):
-    httpserver.expect_request("/templates/").respond_with_json(["http://127.0.0.1:8001/templates/1"])
-    httpserver.expect_request("/templates/1").respond_with_json({'content': 'yourmom'})
+def test_get_all_template_data(httpserver,authorization_header):
+    httpserver.expect_request("/templates/", headers=authorization_header).respond_with_json(["http://127.0.0.1:8001/templates/1"])
+    httpserver.expect_request("/templates/1", headers=authorization_header).respond_with_json({'content': 'yourmom'})
     assert get_all_template_data() == [{'content': 'yourmom'}]
 
 def test_get_template_url():
     assert get_template_url("1_test_123") == "http://127.0.0.1:8001/templates/123"
 
-def test_jsononify_placeholders():
+def test_jsononify_placeholders_valid():
     keys = ['1_content_123', '1_name_123']
     values = [['test1'], ['test2']]
 
@@ -30,3 +33,9 @@ def test_jsononify_placeholders():
         'name': 'test2'
     }
     assert jsonify_placeholders(keys,values) == results
+
+def test_jsononify_placeholders_invalid():
+    keys = ['1_content_123', '1_name_123']
+    values = [['test1'], ['']]
+
+    assert jsonify_placeholders(keys,values) == "Invalid"
