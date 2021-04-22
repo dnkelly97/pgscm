387a25c3d5dbc3930d87d9bdd3ca0ee074c9d70f
Mathew Seng <mathewseng@gmail.com>
2021-04-22 12:04:14 -0500

    Create helper script for making remote dispatch apis

---
+++ pipeline/management/dispatch/dispatch_requests.py
@@ -0,0 +1,34 @@
+import requests
+import os
+
+if 'WEBSITE_HOSTNAME' in os.environ:
+    DISPATCH_URL = "http://127.0.0.1:8000"
+else:
+    DISPATCH_URL = "http://127.0.0.1:8001"
+
+def get_all_template_data():
+    templates = get_templates()
+    print(templates)
+    template_objects = []
+    for template in templates:
+        template_data = get_template(template)
+        template_objects.append(template_data)
+    return template_objects
+
+def get_templates():
+    template_urls = requests.get(DISPATCH_URL+'/templates/').json()
+    return template_urls
+
+def get_template(url):
+    return requests.get(url).json()
+
+def get_template_url(key):
+    id = key.split('_')[2]
+    return DISPATCH_URL+'/templates/'+id
+
+def jsonify_placeholders(keys,values):
+    placeholders = {}
+    for i in range(len(keys)):
+        key = keys[i].split('_')[1]
+        placeholders[key] = values[i][0]
+    return placeholders
