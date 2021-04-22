03b5c05f911f4b6f250464fbe2684c7702be514d
Mathew Seng <mathewseng@gmail.com>
2021-04-22 16:29:53 -0500

    Adjust dispatch request to contain the api key for the client
    For testing you have to set it to a blank value. For interacting with dispatch locally, you set it to the key generated in dispatch

--- pipeline/management/dispatch/dispatch_requests.py
+++ pipeline/management/dispatch/dispatch_requests.py
@@ -2,10 +2,10 @@ import requests
 import os
 
 DISPATCH_URL = "" if 'WEBSITE_HOSTNAME' in os.environ else "http://127.0.0.1:8001"
+DISPATCH_AUTH = "https://dispatchlite.azurewebsites.net/" if 'WEBSITE_HOSTNAME' in os.environ else "" #your testing url, leave blank for testing
 
 def get_all_template_data():
     templates = get_templates()
-    print(templates)
     template_objects = []
     for template in templates:
         template_data = get_template(template)
@@ -13,11 +13,11 @@ def get_all_template_data():
     return template_objects
 
 def get_templates():
-    template_urls = requests.get(DISPATCH_URL+'/templates/').json()
+    template_urls = requests.get(DISPATCH_URL+'/templates/' , headers={'Authorization': 'x-dispatch-api-key DISPATCH_AUTH'}).json()
     return template_urls
 
 def get_template(url):
-    return requests.get(url).json()
+    return requests.get(url, headers={'Authorization': 'x-dispatch-api-key DISPATCH_AUTH'}).json()
 
 def get_template_url(key):
     id = key.split('_')[2]
@@ -27,5 +27,9 @@ def jsonify_placeholders(keys,values):
     placeholders = {}
     for i in range(len(keys)):
         key = keys[i].split('_')[1]
-        placeholders[key] = values[i][0]
+        value = values[i][0]
+        if value == '':
+            return "Invalid"
+        else:
+            placeholders[key] = value
     return placeholders
