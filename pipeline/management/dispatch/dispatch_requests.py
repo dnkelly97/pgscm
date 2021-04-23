271107b4498406d24254a5afc5193a82ec90c030
dnkelly97 <dnkelly97@gmail.com>
2021-04-23 14:52:44 -0500

    Added functions that run post requests to dispatch for creating a campaign and creating a communication

--- pipeline/management/dispatch/dispatch_requests.py
+++ pipeline/management/dispatch/dispatch_requests.py
@@ -1,8 +1,10 @@
 import requests
 import os
+from django.contrib.auth.models import User
+
 
 DISPATCH_URL = "" if 'WEBSITE_HOSTNAME' in os.environ else "http://127.0.0.1:8001" #make sure dispatch runs on port 8001
-DISPATCH_AUTH = "https://dispatchlite.azurewebsites.net/" if 'WEBSITE_HOSTNAME' in os.environ else "" #your developing client key, leave blank for testing
+DISPATCH_AUTH = "https://dispatchlite.azurewebsites.net/" if 'WEBSITE_HOSTNAME' in os.environ else "d3juGGzfHpBVtK1ZmrND" #your developing client key, leave blank for testing
 
 def get_all_template_data():
     templates = get_templates()
@@ -33,3 +35,37 @@ def jsonify_placeholders(keys,values):
         else:
             placeholders[key] = value
     return placeholders
+
+
+def dispatch_campaign_post(pipeline_id, pipeline_name):
+    data = {'id': pipeline_id, 'name': pipeline_name}
+    return requests.post(DISPATCH_URL+'/campaigns/',
+                         headers={'Authorization': 'x-dispatch-api-key ' + DISPATCH_AUTH},
+                         data=data)
+
+
+def dispatch_communication_post(campaign_id, stage_id, name, placeholders, template, campaign):
+    email = {'fromAddress': 'dontmatter@gmail.com', 'fromName': 'you mom. hah.', 'subject': 'this one is a bit of a problem...'}
+    destinations = [
+        {
+          "bounceAddress": "bounce-to-here@uiowa.edu",
+          "linkTrackingDisabled": False,
+          "openTrackingDisabled": False,
+          "replyToAddress": "reply-to-here@uiowa.edu",
+          "suppressionList": "http://garby.net/supressionlists/7684916",
+          "type": "SMTP"
+        }
+    ]
+    data = {'id': stage_id,
+            'name': name,
+            'type': 'EMAIL',
+            'email': email,
+            'destinations': destinations,
+            'placeholders': placeholders,
+            'template': template,
+            'notificationAddresses': [user.email for user in User.objects.filter(is_superuser=True)],
+            'campaign': campaign,
+            }
+    return requests.post(DISPATCH_URL+'/campaigns/' + str(campaign_id) + '/communications',
+                         headers={'Authorization': 'x-dispatch-api-key ' + DISPATCH_AUTH},
+                         json=data)
