fda0922626e59459608a1a3ef22d3887a96b452d
Amar Vukovic <ammaar_1999@hotmail.com>
2021-04-29 10:25:39 -0500

    Updating filters following the small changes to how gpa is calculated.

--- student/filters.py
+++ student/filters.py
@@ -26,8 +26,8 @@ class StudentFilter(django_filters.FilterSet):
     research_interests = CharFilter(method=query_interest, label="Research of Interest")
     university = CharFilter(method=query_uni, label="University")
     degree = CharFilter(method=query_degree, label="Degree")
-    gpa_start = NumberFilter(field_name="gpa", lookup_expr='gte')
-    gpa_end = NumberFilter(field_name="gpa", lookup_expr='lte')
+    gpa_start = NumberFilter(field_name="normal_gpa", lookup_expr='gte')
+    gpa_end = NumberFilter(field_name="normal_gpa", lookup_expr='lte')
 
     class Meta:
         model = Student
