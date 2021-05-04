5f829ec26d70734839cf127ff95fdc111f99786e
dnkelly97 <dnkelly97@gmail.com>
2021-05-04 00:50:14 -0500

    Some progress on pipeline executor code and testing. Next step is to add batch_id and member_id for each member from the batch response to the appropriate student_stage

--- pipeline/tasks.py
+++ pipeline/tasks.py
@@ -1,5 +1,6 @@
 from pipeline.models import Pipeline, Stage, StudentStage
 from pipeline.management.dispatch.dispatch_requests import *
+import json
 
 
 def pipeline_executor():
@@ -10,12 +11,17 @@ def pipeline_executor():
         for stage in pipeline_stages:
             batch = []
             student_stages = StudentStage.objects.filter(stage=stage.id)
-            print([ss.student.name for ss in student_stages])
+            print([ss.student.first_name for ss in student_stages])
             for student_stage in student_stages:
-                pass
-                # check if student has met advancement conditions
-                # if so, add to batch
-            # send batch
+                if student_stage.should_advance():
+                    student_stage.advance_student()
+                    batch.append(student_stage)
+            if batch:
+                batch_url = json.loads(dispatch_adhoc_communications_post(stage.id + 1, batch).content)
+                batch_response = json.loads(dispatch_batch_get(batch_url).content)
+                # TODO: use batch response to add batch id and member id to each student_stage in batch
+
+
             # get batch and update each student's member id in the student_stage
             # for each student in batch, update their student_stage relationship to
             # advance to next stage... anything else need to be updated?
---
+++ test/unit/pipeline/test_tasks.py
@@ -0,0 +1,61 @@
+import pytest
+from django.urls import reverse
+from pipeline.views import *
+from pytest_django.asserts import assertTemplateUsed
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from factories import SavedQueryFactory, PipelineFactory
+from pipeline.models import Pipeline, SavedQuery, StudentStage
+from student.models import Student
+from django.template.loader import render_to_string
+import json
+from pipeline.management.dispatch.dispatch_requests import *
+from pytest_httpserver import httpserver
+import datetime
+from pipeline.tasks import *
+
+
+@pytest.fixture
+def pipeline_setup(db):
+    active_pipelines = Pipeline.objects.filter(active=True)
+    for pipeline in active_pipelines:
+        pipeline.active = False
+        pipeline.save()
+    pipeline = Pipeline.objects.create(name='executor pytest pipeline', num_stages=3, active=True)
+    stage1 = Stage.objects.get(pipeline=pipeline.id, name='Stage 1')
+    stage2 = Stage.objects.get(pipeline=pipeline.id, name='Stage 2')
+    student1 = Student.objects.create(first_name='david', last_name='gilmour', email='dg@gmail.com')
+    student2 = Student.objects.create(first_name='jeff', last_name='beck', email='jb@gmail.com')
+    student3 = Student.objects.create(first_name='jimmy', last_name='page', email='jp@gmail.com')
+    student_stage1 = StudentStage.objects.create(student=student1, stage=stage1, date_joined=datetime.date(2018, 1, 1))
+    student_stage2 = StudentStage.objects.create(student=student2, stage=stage1, date_joined=datetime.date(2018, 1, 1))
+    student_stage3 = StudentStage.objects.create(student=student3, stage=stage2, date_joined=datetime.date(2018, 1, 1))
+    yield student_stage1, student_stage2, student_stage3
+
+
+def test_pipeline_executor_dispatch_requests(pipeline_setup, httpserver, authorization_header):
+    student_stage1, student_stage2, student_stage3 = pipeline_setup
+    communication_id_1 = student_stage1.stage.id + 1
+    communication_id_2 = student_stage3.stage.id + 1
+    expected_data_1 = {
+        'members': [
+            {'toName': 'david gilmour', 'toAddress': 'dg@gmail.com'},
+            {'toName': 'jeff beck', 'toAddress': 'jb@gmail.com'},
+            # {'toName': 'jimmy page', 'toAddress': 'jp@gmail.com'}
+        ],
+        'includeBatchResponse': True
+    }
+    expected_data_2 = {
+        'members': [
+            {'toName': 'jimmy page', 'toAddress': 'jp@gmail.com'}
+        ],
+        'includeBatchResponse': True
+    }
+    batch_response_1 = {}  # TODO
+    batch_response_2 = {}  # TODO
+    httpserver.expect_request(f"/communications/{communication_id_1}/adhocs", headers=authorization_header, json=expected_data_1).respond_with_json("http://127.0.0.1:8001/batches/5")
+    httpserver.expect_request("/batches/5", headers=authorization_header).respond_with_json(batch_response_1)
+    httpserver.expect_request(f"/communications/{communication_id_2}/adhocs", headers=authorization_header, json=expected_data_2).respond_with_json("http://127.0.0.1:8001/batches/6")
+    httpserver.expect_request("/batches/6", headers=authorization_header).respond_with_json(batch_response_2)
+    pipeline_executor()
+    httpserver.check_assertions()
