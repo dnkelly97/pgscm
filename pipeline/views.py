cee71938aa555e8bbeb6d2f44c098e233a695c53
dnkelly97 <dnkelly97@gmail.com>
2021-05-05 10:29:50 -0500

    bug fix from merging main

--- pipeline/views.py
+++ pipeline/views.py
@@ -55,7 +55,7 @@ def create_pipeline(request):
             stage_form = UpdateStageForm(fields, instance=stages[i])
             if stage_form.is_valid():
                 obj = stage_form.save()
-                response = dispatch_communication_post(pipeline.id, post['subject'][i], obj.id, obj.name, obj.placeholders, obj.template_url)
+                response = dispatch_communication_post(pipeline.id, post['subject'][i - 1], obj.id, obj.name, obj.placeholders, obj.template_url)
                 # if response.status_code != 201:
                 #     pipeline.delete()
                 #     return JsonResponse({'success': False, 'message': 'While creating this pipeline a problem occurred with Dispatch.'})
